17111a946a730d714543e35fcd8c0d37cd012e83
saif71 <epbd.saif@gmail.com>
2020-01-24 19:16:15 +0600

    initial push

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+// READ FILES FROM THE GALLERY FOLDER
+$dir = __DIR__ . DIRECTORY_SEPARATOR . "gallery" . DIRECTORY_SEPARATOR;
+$images = glob($dir . "*.{jpg,jpeg,gif,png,svg}", GLOB_BRACE); //add / remove any file format you want to count on. 
+
+// DRAW HTML ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Salman Hossain Saif | App Icons Portfolios</title>
+    <!-- cdnjs -->
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
+    <script src="node_modules/lazyload/lazyload.js"></script>
+            
+  </head>
+  <body>
+  <style>
+    
+  /* [SITE] */
+  body, html {
+
+padding: 30px;
+margin: 0;
+background: url('bg.png') fixed;
+
+}
+
+/* [GALLERY] */
+#gallery {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+#gallery img {
+    box-sizing: border-box;
+    width: 150px;
+    height: 150px;
+    margin: 10px;
+    object-fit: cover;
+    border-radius: 30px;
+    box-shadow: 3px 10px 14px 
+    rgba(0, 0, 0, 0.25);
+    background: white;
+}
+
+/* [RESPONSIVE GALLERY] */
+/* @media screen and (max-width: 850px) {
+  #gallery img {
+    width: 33%;
+  }
+}
+
+@media screen and (max-width: 640px) {
+  #gallery img {
+    width: 50%;
+  }
+} */
+
+
+  </style>
+    <!-- [THE GALLERY] -->
+    <div id="gallery"><?php
+    foreach ($images as $i) {
+      printf("<img class='lazyload' src='load.gif' data-src='gallery/%s'/>", basename($i));
+    }
+    ?></div>
+
+
+
+<script>
+  lazyload();
+
+           </script>  
+  </body>
+</html>
---
+++ readme.md
@@ -0,0 +1,10 @@
+**Installation**
+
+    $ git clone https://github.com/saif71/simple-photo-gallery.git
+    $ cd simple-photo-gallery
+    $ npm install
+
+**Dependencies**
+
+    "jquery": "^3.4.1",
+    "lazyload": "^2.0.0-rc.2"
